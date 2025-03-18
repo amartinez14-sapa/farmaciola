291ad6a29e65515adedd9733a3bd0f96961edf06
a.martinez14 <161350306+amartinez14-sapa@users.noreply.github.com>
2025-03-18 16:05:30 +0100

    Update main.tf

--- Docu/Terraform/fitxers/main.tf
+++ Docu/Terraform/fitxers/main.tf
@@ -1,44 +1,72 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
-      version = "2.9.14"
+      source  = "bpg/proxmox"
+      version = "0.73.1"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://pve1.almaru.es:8006/api2/json"
-  pm_api_token_id     = "tf_user@pve!terraform"
-  pm_api_token_secret = "b2443db6-4c99-4c2f-b2c4-cc62689522f5"
-  pm_tls_insecure     = true
+  endpoint  = "https://192.168.1.5:8006/"
+  username  = "root@pam"
+  password  = "Contraseña"
+  insecure  = true
 }
 
-resource "proxmox_lxc" "test_server" {
-  count    = 3
-  hostname = "mytest-${count.index + 1}"
-  vmid     = count.index + 4001
-  target_node = "pv1"
-  ostemplate  = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
-  unprivileged = true
+resource "proxmox_virtual_environment_vm" "debian_vm" {
+  # Indica quantes VMs vols crear
+  count = 3
+  # Afegeix un sufix o prefix al nom per evitar col·lisions
+  name      = "test-debian-${count.index + 1}"
+  node_name = "pv1"
 
-  cores  = 2
-  memory = 4096
+  machine         = "q35"
+  bios            = "ovmf"
+  stop_on_destroy = true
 
-  rootfs {
-    storage = "Datos"
-    size    = "16G"
+  # Exemple de cloud-init
+  initialization {
+    user_account {
+      username = "user"
+      password = "1234"
+      keys     = [file("~/.ssh/id_rsa.pub")]
+    }
+    ip_config {
+      ipv4 {
+        # Pots modificar l'adreça IP de forma dinàmica si et convé
+        address = "192.168.1.${220 + count.index}/24"
+        gateway = "192.168.1.1"
+      }
+    }
   }
 
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "192.168.1.${count.index + 200}/24"
-    gw     = "192.168.1.1"
+  # Definició de la NIC de xarxa
+  network_device {
+    model   = "virtio"
+    bridge  = "vmbr0"
   }
 
-  start = true
-
-  # Agregar clave SSH al contenedor
-  ssh_public_keys = file("~/.ssh/id_rsa.pub")
+  # CPU
+  cpu {
+    cores   = 4
+    sockets = 1
+    type    = "host"
   }
+
+  # RAM en MB
+  memory {
+    dedicated = 4096
+  }
+
+  # Configuració del disc
+  disk {
+    datastore_id = "Datos"
+    file_id      = "local:iso/jammy-server-cloudimg-amd64.img"
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 16
+    file_format  = "raw"
+  }
+}
